0c563bc67205ea1e08b424e22d0c08262bdd004c
Breadfest <ethanoskatt1@gmail.com>
2025-11-18 10:20:39 -0800

    copy elevator from nessi;

---
+++ Ache-2026/src/main/java/frc/team4276/frc2025/subsystems/elevator/Elevator.java
@@ -0,0 +1,168 @@
+package frc.team4276.frc2025.subsystems.elevator;
+
+import static frc.team4276.frc2025.subsystems.elevator.ElevatorConstants.*;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team4276.frc2025.Constants;
+import frc.team4276.lib.dashboard.LoggedTunableProfile;
+import java.util.function.BooleanSupplier;
+import org.littletonrobotics.junction.Logger;
+
+public class Elevator extends SubsystemBase {
+  private ElevatorPosition wantedElevatorPose = ElevatorPosition.STOW;
+
+  public enum WantedState {
+    IDLE,
+    MOVE_TO_POSITION,
+    CUSTOM
+  }
+
+  private enum SystemState {
+    IDLING,
+    MOVING_TO_POSITION,
+    CUSTOM
+  }
+
+  private WantedState wantedState = WantedState.IDLE;
+  private SystemState systemState = SystemState.IDLING;
+
+  private final ElevatorIO io;
+  private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+
+  private ElevatorFeedforward ff =
+      new ElevatorFeedforward(
+          kS.getAsDouble(), kG.getAsDouble(), kV.getAsDouble(), kA.getAsDouble());
+  private final LoggedTunableProfile profile = new LoggedTunableProfile("Elevator", 1.5, 20.0);
+  private TrapezoidProfile.State setpointState = new TrapezoidProfile.State();
+
+  private BooleanSupplier coastOverride = () -> false;
+
+  /** position that reads zero on the elevator */
+  private double homedPosition = 0.0;
+
+  public Elevator(ElevatorIO io) {
+    this.io = io;
+    io.setBrakeMode(true);
+  }
+
+  public void setCoastOverride(BooleanSupplier coastOverride) {
+    this.coastOverride = coastOverride;
+  }
+
+  private boolean hasFlippedCoast = false;
+  private boolean wasDisabled = true;
+
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Elevator", inputs);
+
+    if (inputs.botLimit) {
+      homedPosition = inputs.position;
+    }
+
+    systemState = handleStateTransitions();
+    applyStates();
+
+    if (DriverStation.isDisabled()) {
+      wasDisabled = true;
+
+      io.stop();
+
+      if (!coastOverride.getAsBoolean()) {
+        hasFlippedCoast = true;
+      }
+
+      io.setBrakeMode(!(coastOverride.getAsBoolean() && hasFlippedCoast));
+
+      setpointState = new TrapezoidProfile.State(getPositionMetres(), 0.0);
+
+      if (Constants.isTuning) {
+        ff =
+            new ElevatorFeedforward(
+                kS.getAsDouble(), kG.getAsDouble(), kV.getAsDouble(), kA.getAsDouble());
+      }
+
+    } else {
+      if (wasDisabled) {
+        io.setBrakeMode(true);
+        wasDisabled = false;
+        hasFlippedCoast = false;
+      }
+
+      Logger.recordOutput("Elevator/SetpointState/PosMetres", setpointState.position);
+      Logger.recordOutput("Elevator/SetpointState/VelMetres", setpointState.velocity);
+    }
+
+    Logger.recordOutput("Elevator/WantedElevatorPose", wantedElevatorPose);
+    Logger.recordOutput("Elevator/AtGoal", atGoal());
+    Logger.recordOutput("Elevator/HomedPositionRotation", homedPosition);
+    Logger.recordOutput("Elevator/PositionMetres", getPositionMetres());
+  }
+
+  private SystemState handleStateTransitions() {
+    return switch (wantedState) {
+      case IDLE:
+        yield SystemState.IDLING;
+      case MOVE_TO_POSITION:
+        yield SystemState.MOVING_TO_POSITION;
+      case CUSTOM:
+        yield SystemState.CUSTOM;
+    };
+  }
+
+  private void applyStates() {
+    switch (systemState) {
+      case IDLING:
+        io.runVolts(0.0);
+
+        break;
+
+      case MOVING_TO_POSITION:
+        setpointState =
+            profile.calculate(
+                0.02,
+                setpointState,
+                new TrapezoidProfile.State(wantedElevatorPose.getPositionMetres(), 0.0));
+        double setpointRotations =
+            metresToRotations(MathUtil.clamp(setpointState.position, minInput, maxInput))
+                + homedPosition;
+        io.runSetpoint(setpointRotations, ff.calculate(setpointState.velocity));
+
+        break;
+
+      case CUSTOM:
+        break;
+    }
+  }
+
+  public void setWantedState(WantedState wantedState, ElevatorPosition elevatorPosition) {
+    this.wantedState = wantedState;
+    this.wantedElevatorPose = elevatorPosition;
+  }
+
+  public ElevatorPosition getWantedElevatorPose() {
+    return wantedElevatorPose;
+  }
+
+  public boolean atGoal() {
+    return Constants.isSim
+        ? true
+        : MathUtil.isNear(wantedElevatorPose.getPositionMetres(), getPositionMetres(), tolerance);
+  }
+
+  public static double metresToRotations(double metres) {
+    return (metres / drumCircumference) * gearRatio;
+  }
+
+  public static double rotationsToMetres(double rotations) {
+    return (rotations / gearRatio) * drumCircumference;
+  }
+
+  public double getPositionMetres() {
+    return rotationsToMetres(inputs.position) - rotationsToMetres(homedPosition);
+  }
+}
---
+++ Ache-2026/src/main/java/frc/team4276/frc2025/subsystems/elevator/ElevatorConstants.java
@@ -0,0 +1,90 @@
+package frc.team4276.frc2025.subsystems.elevator;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import frc.team4276.frc2025.Ports;
+import frc.team4276.lib.dashboard.LoggedTunableNumber;
+import java.util.function.DoubleSupplier;
+
+public class ElevatorConstants {
+  public static final LoggedTunableNumber stow =
+      new LoggedTunableNumber("Elevator/StowPosition", Units.inchesToMeters(0.5));
+  public static final LoggedTunableNumber intake =
+      new LoggedTunableNumber("Elevator/IntakePosition", Units.inchesToMeters(0.0));
+  public static final LoggedTunableNumber unjam =
+      new LoggedTunableNumber("Elevator/UnjamPosition", Units.inchesToMeters(15.0));
+  public static final LoggedTunableNumber l1 =
+      new LoggedTunableNumber("Elevator/L1Position", Units.inchesToMeters(0.0));
+  public static final LoggedTunableNumber l2 =
+      new LoggedTunableNumber("Elevator/L2Position", Units.inchesToMeters(6.85));
+  public static final LoggedTunableNumber l3 =
+      new LoggedTunableNumber("Elevator/L3Position", Units.inchesToMeters(22.5));
+  public static final LoggedTunableNumber lowAlgae =
+      new LoggedTunableNumber("Elevator/LoAlgae", Units.inchesToMeters(10.0));
+  public static final LoggedTunableNumber highAlgae =
+      new LoggedTunableNumber("Elevator/HiAlgae", Units.inchesToMeters(17.32));
+  public static final LoggedTunableNumber algaeChop =
+      new LoggedTunableNumber("Elevator/AlgaeChop", Units.inchesToMeters(0.0));
+  public static final LoggedTunableNumber climb =
+      new LoggedTunableNumber("Elevator/Climb", Units.inchesToMeters(0.0));
+
+  public enum ElevatorPosition {
+    STOW(stow),
+    INTAKE(intake),
+    UNJAM(unjam),
+    L1(l1),
+    L2(l2),
+    L3(l3),
+    LOW_ALGAE(lowAlgae),
+    HIGH_ALGAE(highAlgae),
+    ALGAE_CHOP(algaeChop),
+    CLIMB(climb);
+
+    private final DoubleSupplier elevatorSetpointSupplier;
+
+    private ElevatorPosition(DoubleSupplier elevatorSetpointSupplier) {
+      this.elevatorSetpointSupplier = elevatorSetpointSupplier;
+    }
+
+    public double getPositionMetres() {
+      return elevatorSetpointSupplier.getAsDouble();
+    }
+  }
+
+  public static final int leaderId = Ports.ELEVATOR_LEADER;
+  public static final int followerId = Ports.ELEVATOR_FOLLOWER;
+
+  public static final boolean invertLeader = true;
+  public static final boolean invertFollower = true;
+
+  public static final int currentLimit = 50;
+
+  public static final double drumCircumference = Units.inchesToMeters(0.25 * 22); // m
+  public static final double drumDiameter = drumCircumference / Math.PI; // m
+  public static final double encoderPositionFactor = 1.0;
+  public static final double encoderVelocityFactor = 1.0 / 60.0;
+  public static final boolean invertEncoder = true;
+
+  public static final double kp = 0.1;
+  public static final double ki = 0.0;
+  public static final double kd = 0.0;
+
+  public static final int readFreq = 50;
+
+  public static final double minInput = 0.0; // m
+  public static final double maxInput = Units.inchesToMeters(25.5); // m
+
+  public static final double gearRatio = 5.0;
+  public static final Translation2d origin =
+      new Translation2d(0.0, -Units.inchesToMeters(0.860191));
+  public static final double length = Units.inchesToMeters(35);
+
+  public static final double maxPosition = Units.inchesToMeters(25.5); // m
+
+  public static final double tolerance = 0.01; // m
+
+  public static final LoggedTunableNumber kS = new LoggedTunableNumber("Elevator/kS", 0.0);
+  public static final LoggedTunableNumber kV = new LoggedTunableNumber("Elevator/kV", 4.38);
+  public static final LoggedTunableNumber kG = new LoggedTunableNumber("Elevator/kG", 0.52);
+  public static final LoggedTunableNumber kA = new LoggedTunableNumber("Elevator/kA", 0.08);
+}
