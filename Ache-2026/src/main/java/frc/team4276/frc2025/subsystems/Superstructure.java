37d5a999e5bdcfed6926474852798448f5be4da6
eBulosan <122693686+eBulosan@users.noreply.github.com>
2025-11-22 17:51:15 -0800

    superstructure work

--- Ache-2026/src/main/java/frc/team4276/frc2025/subsystems/Superstructure.java
+++ Ache-2026/src/main/java/frc/team4276/frc2025/subsystems/Superstructure.java
@@ -1,5 +1,7 @@
 package frc.team4276.frc2025.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team4276.frc2025.subsystems.drive.Drive;
 import frc.team4276.frc2025.subsystems.elevator.Elevator;
@@ -18,13 +20,34 @@ public class Superstructure extends SubsystemBase {
   private final ViXController controller;
 
   public enum WantedSuperState {
-
+    STOW,
+    REEF_TEST,
+    INTAKE_CORAL,
+    SCORE_L1_INTAKE,
+    PURGE
   }
 
   public enum CurrentSuperState {
-
+    STOWED,
+    REEF_TESTING,
+    INTAKING_CORAL,
+    SCORING_L1_INTAKE,
+    PURGING
   }
 
+  private WantedSuperState wantedSuperState = WantedSuperState.STOW;
+  private CurrentSuperState currentSuperState = CurrentSuperState.STOWED;
+
+  public enum GamePieceState {
+    NO_BANANA,
+    CORAL,
+    ALGAE
+  }
+
+  private GamePieceState gamePieceState = GamePieceState.NO_BANANA;
+
+  private boolean isL1Mode = true;
+  
   public Superstructure(Drive drive, Vision vision, Elevator elevator, Intake intake, ViXController controller){
     this.drive = drive;
     this.vision = vision;
@@ -35,6 +58,37 @@ public class Superstructure extends SubsystemBase {
 
   @Override
   public void periodic() {
-      
+    
+      if(intake.hasCoral()){
+
+      } else {
+        gamePieceState = GamePieceState.NO_BANANA;
+
+      }
+
+    currentSuperState = handleStateTransition();
+    applyState();
+
+    Logger.recordOutput("Superstructure/WantedSuperState", wantedSuperState);
+    Logger.recordOutput("Superstructure/CurrentSuperState", currentSuperState);
+  }
+
+  private CurrentSuperState handleStateTransition(){
+    return switch (wantedSuperState) {
+      case STOW: 
+        yield CurrentSuperState.STOWED;
+      case REEF_TEST: 
+        yield CurrentSuperState.REEF_TESTING;
+      case INTAKE_CORAL: 
+        yield CurrentSuperState.INTAKING_CORAL;
+      case SCORE_L1_INTAKE: 
+        yield CurrentSuperState.SCORING_L1_INTAKE;
+      case PURGE: 
+        yield CurrentSuperState.PURGING;
+    };
+  }
+
+  private void applyState(){
+
   }
 }
